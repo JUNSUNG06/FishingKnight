f6f4c760b6ffb8b4d24cbf2ec79337fc754eacfd
JUNSUNG06 <forest2847@gmail.com>
2024-08-11 16:45:38 +0900

    turn

--- FishingKnight/Assets/01.Scripts/Content/EntityComponent/Character/CharacterMovement.cs
+++ FishingKnight/Assets/01.Scripts/Content/EntityComponent/Character/CharacterMovement.cs
@@ -9,27 +9,45 @@ public class CharacterMovement : CharacterComponent
 {
     private Rigidbody rb;
 
+    //move
     [SerializeField] private float maxMoveSpeed;
     [SerializeField] private float acceleation;
     private Coroutine accelerationCo;
     private float moveSpeed;
     private Vector3 moveDir;
     private Vector3 prevMoveDir;
+
     public float MoveSpeed
     {
         get { return moveSpeed; } 
-        set
+        private set
         {
-            OnMoveSpeedChanged?.Invoke(moveSpeed, value);
-
+            float prev = moveSpeed;
             moveSpeed = value;
+
+            OnMoveSpeedChanged?.Invoke(prev, moveSpeed);
         }
     }
     public float MaxMoveSpeed => maxMoveSpeed;
-    public Vector3 MoveDirection => moveDir;
+    public Vector3 MoveDirection
+    {
+        get { return moveDir; }
+        private set
+        {
+            prevMoveDir = moveDir;
+            moveDir = value;
+
+            OnMoveDirectionChanged?.Invoke(PrevMoveDirection, MoveDirection);
+        }
+    }
     public Vector3 PrevMoveDirection => prevMoveDir;
 
     public Action<float, float/*prev, new*/> OnMoveSpeedChanged;
+    public Action<Vector3, Vector3/*prev, new*/> OnMoveDirectionChanged;
+
+    //turn
+    [SerializeField] private float turnSpeed;
+    private Coroutine turnCo;
 
     public override void Initialize(Entity owner)
     {
@@ -57,8 +75,7 @@ public class CharacterMovement : CharacterComponent
 
     public void SetMoveDirection(Vector3 direction)
     {
-        prevMoveDir = moveDir;
-        moveDir = direction.normalized;
+        MoveDirection = direction.normalized;
     }
 
     public void SetMoveSpeed(float moveSpeedWeight, bool acceleation = true)
@@ -94,5 +111,48 @@ public class CharacterMovement : CharacterComponent
         MoveSpeed = speed;
 
         accelerationCo = null;
+    }
+
+    public void LookMoveDirection()
+    {
+        Turn(MoveDirection);
+    }
+
+    public void Turn(Vector3 direction)
+    {
+        if (!gameObject.activeSelf)
+            return;
+
+        direction.Normalize();
+
+        if (direction != Vector3.zero)
+        {
+            if (turnCo != null)
+                StopCoroutine(turnCo);
+            turnCo = StartCoroutine(TurnCo(direction));
+        }
+        else
+        {
+            if (turnCo != null)
+                StopCoroutine(turnCo);
+        }
+    }
+
+    private IEnumerator TurnCo(Vector3 direction)
+    {
+        float t = 0f;
+        Quaternion start = transform.rotation;
+        Quaternion end = Quaternion.AngleAxis(Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, Vector3.up);
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * turnSpeed;
+            transform.rotation = Quaternion.Lerp(start, end, t);
+
+            yield return null;
+        }
+        transform.rotation = end;
+
+        turnCo = null;
     }
 }
--- FishingKnight/Assets/01.Scripts/Content/EntityComponent/Character/FSM/Actions/MovementAction.cs
+++ FishingKnight/Assets/01.Scripts/Content/EntityComponent/Character/FSM/Actions/MovementAction.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class MovementAction : FSMAction
 {
     [Range(0f, 1f)][SerializeField] private float moveSpeedWeight;
+    [SerializeField] private bool turn = true;
 
     public override void EnterState()
     {
         character.Movement.OnMoveSpeedChanged += OnMoveSpeedChange;
         character.Movement.SetMoveSpeed(moveSpeedWeight);
+
+        character.Movement.OnMoveDirectionChanged += OnMoveDirectionChanged;
     }
 
     public override void UpdateState()
@@ -22,11 +25,20 @@ public class MovementAction : FSMAction
     public override void ExitState()
     {
         character.Movement.OnMoveSpeedChanged -= OnMoveSpeedChange;
+        character.Movement.OnMoveDirectionChanged -= OnMoveDirectionChanged;
     }
 
     private void OnMoveSpeedChange(float prev, float next)
     {
         float threshold = next / character.Movement.MaxMoveSpeed;
         character.Anim.Animator.SetFloat("move_speed", threshold);
+    }
+
+    private void OnMoveDirectionChanged(Vector3 prev, Vector3 next)
+    {
+        if(turn)
+        {
+            character.Movement.LookMoveDirection();
+        }
     }
 }
